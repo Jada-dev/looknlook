bfdad23616f1750cec1b6b43bf94a65cdd6f9492
Nikhi Jada <hello@jada.dev>
2023-04-06 14:32:30 +0530

    latest commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:looknlook/splash.dart';
 
-
 Future<void> main() async {
-
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
-// Ideal time to initialize
 
   runApp(const MyApp());
 }
